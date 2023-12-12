cf49ce9f635af2895d08fafae389d25fee0d3504
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2023-12-12 12:45:00 +0000

    Add a README and LICENSE

---
+++ README.md
@@ -0,0 +1,14 @@
+# check-stacks
+
+A simple little script that will iterate all accounts and find stacks, creating a CSV report.
+
+Entirely meaningless outside GOV.UK One Login.
+
+To use:
+
+- `pip3 install boto3`
+- run `./main.py`
+- you'll be prompted to log in and authorize the application in AWS Identity Center
+- a few minutes later, stacks.csv will appear, with all that juicy data goodness
+
+Enjoy, I guess?
--- cloudformation.py
+++ cloudformation.py
@@ -1,10 +1,11 @@
 from boto3.session import Session
 
+
 def list_stacks(session: Session):
-  cfn = session.client('cloudformation')
+    cfn = session.client("cloudformation")
 
-  paginator = cfn.get_paginator('describe_stacks').paginate()
+    paginator = cfn.get_paginator("describe_stacks").paginate()
 
-  for page in paginator:
-    for stack in page['Stacks']:
-      yield stack
+    for page in paginator:
+        for stack in page["Stacks"]:
+            yield stack
